52755949f94f6b93b4f6b252992522474facf6a2
donnydonny123 <don.cricketsss@gmail.com>
2017-05-17 15:14:28 +0800

    Save account and password in local

--- app/src/main/java/com/example/user/asthma/LoginActivity.java
+++ app/src/main/java/com/example/user/asthma/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.asthma;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,16 +8,38 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    String lg_account = "donny";
+    String lg_pwd = "donny";
+    public static final String defaultAccount = "Account";
+
+    private boolean loginVerify(String uid, String pw){
+        //dummy function
+        return (uid.equals(lg_account)&& pw.equals(lg_pwd));
+    }
 
     public void login(View v){
         String uid = ((EditText) findViewById(R.id.account)).getText().toString();
         String pw = ((EditText) findViewById(R.id.password)).getText().toString();
 
-        Toast.makeText(this, "login success", Toast.LENGTH_LONG).show();
-        getIntent().putExtra("LOGIN_USERID", uid);
-        getIntent().putExtra("LOGIN_PASSWD", pw);
-        setResult(RESULT_OK, getIntent());
-        finish();
+        if (loginVerify(uid, pw)){
+            Toast.makeText(this, "login success", Toast.LENGTH_LONG).show();
+            getIntent().putExtra("LOGIN_USERID", uid);
+            getIntent().putExtra("LOGIN_PASSWD", pw);
+            setResult(RESULT_OK, getIntent());
+
+            SharedPreferences accounts = getSharedPreferences(defaultAccount, 0);
+            SharedPreferences.Editor spEditor = accounts.edit();
+            spEditor.putString("account", uid);
+            spEditor.putString("password", pw);
+            spEditor.apply();
+
+            finish();
+        }
+        else {
+            Toast.makeText(this, "login fail", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 
 
@@ -24,5 +47,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText edAccount = (EditText) findViewById(R.id.account);
+        EditText edPwd = (EditText) findViewById(R.id.password);
+        SharedPreferences accounts = getSharedPreferences(defaultAccount, 0);
+        edAccount.setText(accounts.getString("account", ""));
+        edPwd.setText(accounts.getString("password", ""));
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
     }
 }
